7b054868dd2d60512e7ac1baf6f720b19a6eec4b
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-10 21:24:48 +0000

    Add data_analysis class and content

---
+++ lib/data_analysis.rb
@@ -0,0 +1,11 @@
+require_relative 'users'
+require_relative 'purchases'
+
+class DataAnalysis
+  attr_reader :users, :purchases
+
+  def initialize(users = Users.new, purchases=Purchases.new)
+    @users = users
+    @purchases = purchases
+  end
+end
