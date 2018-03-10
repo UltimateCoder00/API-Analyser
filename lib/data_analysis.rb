e4c3e6339e4642222e093aea3d8ad5b722077486
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-10 21:50:57 +0000

    Add most_sold method

--- lib/data_analysis.rb
+++ lib/data_analysis.rb
@@ -8,4 +8,12 @@ class DataAnalysis
     @users = users
     @purchases = purchases
   end
+
+  def most_sold
+    items_sold = {}
+    purchases.json_response["data"].each do |data|
+      items_sold.include?(data["item"]) ? items_sold[data["item"]] += 1 : items_sold[data["item"]] = 1
+    end
+    items_sold.sort_by { |key, val| val }.last.first
+  end
 end
