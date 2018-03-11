bd1c7ba581ab509ba6dd1397a9a5916dba9b659a
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-11 21:06:34 +0000

    Remove sapces

--- lib/users.rb
+++ lib/users.rb
@@ -6,7 +6,6 @@ Envyable.load('config/env.yml')
 class Users
   attr_reader :response, :json_response
 
-
   def initialize
     @response = RestClient.get ENV["USER_DATA_ENDPOINT"]
     @json_response = JSON.parse(response.body)
