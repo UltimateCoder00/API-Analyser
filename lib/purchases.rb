8944883370b02d9fedd2a50e0f3fd41257430cb2
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-11 00:20:17 +0000

    Update with env.yml endpoints

--- lib/purchases.rb
+++ lib/purchases.rb
@@ -1,11 +1,13 @@
 require 'rest_client'
 require 'json'
+require 'envyable'
+Envyable.load('config/env.yml')
 
 class Purchases
   attr_reader :response, :json_response
 
   def initialize
-    @response = RestClient.get 'https://driftrock-dev-test.herokuapp.com/purchases'
+    @response = RestClient.get ENV["PURCHASE_DATA_ENDPOINT"]
     @json_response = JSON.parse(response.body)
   end
 end
--- lib/users.rb
+++ lib/users.rb
@@ -1,11 +1,14 @@
 require 'rest_client'
 require 'json'
+require 'envyable'
+Envyable.load('config/env.yml')
 
 class Users
   attr_reader :response, :json_response
 
+
   def initialize
-    @response = RestClient.get 'https://driftrock-dev-test.herokuapp.com/users'
+    @response = RestClient.get ENV["USER_DATA_ENDPOINT"]
     @json_response = JSON.parse(response.body)
   end
 end
