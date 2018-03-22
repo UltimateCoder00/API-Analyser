328809d5d86ce8bfeba6c5feb193b560a681d62b
Hasan Sonmez <hasansonmez100@hotmail.com>
2018-03-22 15:09:50 +0000

    Add stubbing

--- spec/data_analysis_spec.rb
+++ spec/data_analysis_spec.rb
@@ -1,7 +1,14 @@
 require 'data_analysis'
 
 describe DataAnalysis do
-  subject(:data_analysis) {described_class.new}
+  let(:purchase) {double json_response: {"data"=>[{"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Incredible Iron Pants", "spend"=>"84.93"},
+                                                  {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Incredible Granite Hat", "spend"=>"76.89"},
+                                                  {"user_id"=>"S27G-8UMJ-LDSL-UOPN", "item"=>"Durable Concrete Hat", "spend"=>"15.31"}
+                                                  ]}}
+  let(:user) {double json_response: {"data"=>[{"id"=>"KZHR-1H35-2IH8-JXYN", "first_name"=>"Quincy", "last_name"=>"Schimmel", "phone"=>"186.301.6921 x948", "email"=>"schimmel_quincy@ernser.io"},
+                                              {"id"=>"S27G-8UMJ-LDSL-UOPN", "first_name"=>"Henry", "last_name"=>"Terry", "phone"=>"636-387-6074 x690", "email"=>"terry_henry@doyle.io"}]}}
+
+  subject(:data_analysis) {described_class.new(user, purchase)}
 
   describe '#users' do
     it 'Contains array of users data' do
@@ -17,30 +24,37 @@ describe DataAnalysis do
 
   describe '#most_sold' do
     it 'Returns most sold item' do
-      most_sold_item = "Heavy Duty Concrete Watch"
+      most_sold_item = "Incredible Iron Pants"
       expect(data_analysis.most_sold).to eq most_sold_item
     end
   end
 
   describe '#most_loyal' do
     it 'Returns most loyal customer' do
-      most_sold_loyal_customer = "travis_kshlerin@wunsch.net"
+      most_sold_loyal_customer = "schimmel_quincy@ernser.io"
       expect(data_analysis.most_loyal).to eq most_sold_loyal_customer
+    end
+
+    describe 'When there are equal most loyal users' do
+      let(:purchase) {double json_response: {"data"=>[{"user_id"=>"S27G-8UMJ-LDSL-UOPN", "item"=>"Durable Concrete Hat", "spend"=>"15.31"},
+                                                      {"user_id"=>"S27G-8UMJ-LDSL-UOPN", "item"=>"Fantastic Steel Bottle", "spend"=>"5.64"},
+                                                      {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Incredible Iron Pants", "spend"=>"84.93"},
+                                                      {"user_id"=>"KZHR-1H35-2IH8-JXYN", "item"=>"Incredible Granite Hat", "spend"=>"76.89"}
+                                                      ]}}
+      let(:user) {double json_response: {"data"=>[{"id"=>"KZHR-1H35-2IH8-JXYN", "first_name"=>"Quincy", "last_name"=>"Schimmel", "phone"=>"186.301.6921 x948", "email"=>"schimmel_quincy@ernser.io"},
+                                                  {"id"=>"S27G-8UMJ-LDSL-UOPN", "first_name"=>"Henry", "last_name"=>"Terry", "phone"=>"636-387-6074 x690", "email"=>"terry_henry@doyle.io"}]}}
+
+      it 'Returns first most loyal user' do
+        most_sold_loyal_customer = "terry_henry@doyle.io"
+        expect(data_analysis.most_loyal).to eq most_sold_loyal_customer
+      end
     end
   end
 
   describe '#total_spend(email)' do
     it 'Returns total spend of customer' do
-      email = "travis_kshlerin@wunsch.net"
-      total_spend = 336.76
-      expect(data_analysis.total_spend(email)).to eq total_spend
-
-      email = "eliseo.zieme@bosco.info"
-      total_spend = 49.77
-      expect(data_analysis.total_spend(email)).to eq total_spend
-
-      email = "pearlie.yost@greenholt.biz"
-      total_spend = 303.06
+      email = "terry_henry@doyle.io"
+      total_spend = 15.31
       expect(data_analysis.total_spend(email)).to eq total_spend
     end
 
